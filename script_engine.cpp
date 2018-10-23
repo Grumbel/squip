3cb1d81738ca0f572cb28219e831573366f42548
Ingo Ruhnke <grumbel@gmail.com>
2018-10-23 20:20:29 +0200

    Move Squirrel related code into squirrel/ subdirectory
    The scripting/ directory and namespace shall only be used for code that is
    getting exported to Squirrel, not surrounding utility functions.

---
+++ script_engine.cpp
@@ -0,0 +1,161 @@
+//  SuperTux
+//  Copyright (C) 2018 Ingo Ruhnke <grumbel@gmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "squirrel/script_engine.hpp"
+
+#include "squirrel/script_interface.hpp"
+#include "squirrel/scripting.hpp"
+#include "squirrel/squirrel_error.hpp"
+#include "squirrel/squirrel_util.hpp"
+#include "supertux/game_object.hpp"
+#include "util/log.hpp"
+
+ScriptEngine::ScriptEngine() :
+  m_vm(scripting::global_vm),
+  m_table(),
+  m_scripts()
+{
+  // garbage collector has to be invoked manually
+  sq_collectgarbage(m_vm);
+
+  sq_newtable(m_vm);
+  sq_pushroottable(m_vm);
+  if(SQ_FAILED(sq_setdelegate(m_vm, -2)))
+    throw scripting::SquirrelError(m_vm, "Couldn't set table delegate");
+
+  sq_resetobject(&m_table);
+  if (SQ_FAILED(sq_getstackobj(m_vm, -1, &m_table))) {
+    throw scripting::SquirrelError(m_vm, "Couldn't get table");
+  }
+
+  sq_addref(m_vm, &m_table);
+  sq_pop(m_vm, 1);
+}
+
+ScriptEngine::~ScriptEngine()
+{
+  for(auto& script: m_scripts)
+  {
+    sq_release(m_vm, &script);
+  }
+  m_scripts.clear();
+  sq_release(m_vm, &m_table);
+
+  sq_collectgarbage(m_vm);
+}
+
+void
+ScriptEngine::expose_self(const std::string& name)
+{
+  sq_pushroottable(m_vm);
+  scripting::store_object(m_vm, name.c_str(), m_table);
+  sq_pop(m_vm, 1);
+}
+
+void
+ScriptEngine::unexpose_self(const std::string& name)
+{
+  sq_pushroottable(m_vm);
+  scripting::delete_table_entry(m_vm, name.c_str());
+  sq_pop(m_vm, 1);
+}
+
+void
+ScriptEngine::try_expose(GameObject& object)
+{
+  auto script_object = dynamic_cast<ScriptInterface*>(&object);
+  if (script_object != nullptr) {
+    sq_pushobject(m_vm, m_table);
+    script_object->expose(m_vm, -1);
+    sq_pop(m_vm, 1);
+  }
+}
+
+void
+ScriptEngine::try_unexpose(GameObject& object)
+{
+  auto script_object = dynamic_cast<ScriptInterface*>(&object);
+  if (script_object != nullptr) {
+    SQInteger oldtop = sq_gettop(m_vm);
+    sq_pushobject(m_vm, m_table);
+    try {
+      script_object->unexpose(m_vm, -1);
+    } catch(std::exception& e) {
+      log_warning << "Couldn't unregister object: " << e.what() << std::endl;
+    }
+    sq_settop(m_vm, oldtop);
+  }
+}
+
+void
+ScriptEngine::unexpose(const std::string& name)
+{
+  SQInteger oldtop = sq_gettop(m_vm);
+  sq_pushobject(m_vm, m_table);
+  try {
+    scripting::unexpose_object(m_vm, -1, name.c_str());
+  } catch(std::exception& e) {
+    log_warning << "Couldn't unregister object: " << e.what() << std::endl;
+  }
+  sq_settop(m_vm, oldtop);
+}
+
+void
+ScriptEngine::run_script(const std::string& script, const std::string& sourcename)
+{
+  if (script.empty()) return;
+
+  std::istringstream stream(script);
+  run_script(stream, sourcename);
+}
+
+void
+ScriptEngine::run_script(std::istream& in, const std::string& sourcename)
+{
+  try
+  {
+    // garbage collect thread list
+    for(auto i = m_scripts.begin(); i != m_scripts.end(); ) {
+      HSQOBJECT& object = *i;
+      HSQUIRRELVM vm = scripting::object_to_vm(object);
+
+      if(sq_getvmstate(vm) != SQ_VMSTATE_SUSPENDED) {
+        sq_release(m_vm, &object);
+        i = m_scripts.erase(i);
+        continue;
+      }
+
+      ++i;
+    }
+
+    HSQOBJECT object = scripting::create_thread(m_vm);
+    m_scripts.push_back(object);
+
+    HSQUIRRELVM vm = scripting::object_to_vm(object);
+
+    // set root table
+    sq_pushobject(vm, m_table);
+    sq_setroottable(vm);
+
+    scripting::compile_and_run(vm, in, sourcename);
+  }
+  catch(const std::exception& e)
+  {
+    log_warning << "Error running script: " << e.what() << std::endl;
+  }
+}
+
+/* EOF */
---
+++ script_engine.hpp
@@ -0,0 +1,82 @@
+//  SuperTux
+//  Copyright (C) 2006 Matthias Braun <matze@braunis.de>
+//                2018 Ingo Ruhnke <grumbel@gmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef HEADER_SUPERTUX_SUPERTUX_SCRIPT_ENGINE_HPP
+#define HEADER_SUPERTUX_SUPERTUX_SCRIPT_ENGINE_HPP
+
+#include <string>
+#include <vector>
+
+#include <squirrel.h>
+
+#include "squirrel/squirrel_util.hpp"
+
+class GameObject;
+class ScriptInterface;
+
+class ScriptEngine
+{
+public:
+  ScriptEngine();
+  virtual ~ScriptEngine();
+
+protected:
+  /** Expose this engine in the global_vm under 'name' */
+  void expose_self(const std::string& name);
+  void unexpose_self(const std::string& name);
+
+  /** Expose the GameObject if it has a ScriptInterface, otherwise do
+      nothing. */
+  void try_expose(GameObject& object);
+  void try_unexpose(GameObject& object);
+
+  /** Generic expose function, T must be a type that has a
+      create_squirrel_instance() associated with it. */
+  template<typename T>
+  void expose(const std::string& name, std::unique_ptr<T> script_object)
+  {
+    HSQUIRRELVM vm = scripting::global_vm;
+    sq_pushobject(vm, m_table);
+    scripting::expose_object(vm, -1, std::move(script_object), name.c_str());
+    sq_pop(vm, 1);
+  }
+  void unexpose(const std::string& name);
+
+public:
+  /** Convenience function that takes an std::string instead of an
+      std::istream& */
+  void run_script(const std::string& script, const std::string& sourcename);
+
+  /** Runs a script in the context of the ScriptEngine (m_table will
+      be the roottable of this squirrel VM) and keeps a reference to
+      the script so the script gets destroyed when the ScriptEngine is
+      destroyed). */
+  void run_script(std::istream& in, const std::string& sourcename);
+
+private:
+  HSQUIRRELVM m_vm;
+  HSQOBJECT m_table;
+  std::vector<HSQOBJECT> m_scripts;
+
+private:
+  ScriptEngine(const ScriptEngine&) = delete;
+  ScriptEngine& operator=(const ScriptEngine&) = delete;
+};
+
+#endif
+
+/* EOF */

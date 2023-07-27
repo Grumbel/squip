3ba70476b515604c0fe2bb268a9f7b674ac50d6b
Ingo Ruhnke <grumbel@gmail.com>
2023-07-27 16:34:19 +0200

    Add argument unpacking for uniform arrays

--- include/squip/unpack.hpp
+++ include/squip/unpack.hpp
@@ -17,8 +17,10 @@
 #ifndef UNPACK_HPP
 #define UNPACK_HPP
 
-#include <squirrel.h>
 #include <iostream>
+#include <vector>
+
+#include <squirrel.h>
 
 #include "squip/squirrel_error.hpp"
 
@@ -65,6 +67,49 @@ SQChar const* unpack<SQChar const*>(HSQUIRRELVM vm, SQInteger idx)
     throw SquirrelError(vm, "failed to retrieve string");
   }
   return value;
+}
+
+template<typename T>
+std::vector<T> unpack_array(HSQUIRRELVM vm, SQInteger idx)
+{
+  std::vector<T> value;
+
+  sq_pushnull(vm);  // iterator
+  while (SQ_SUCCEEDED(sq_next(vm, idx)))
+  {
+    // here -1 is the value and -2 is the key
+    // we ignore the key, since that is just the index in an array
+    value.emplace_back(unpack<T>(vm, -1));
+
+    sq_pop(vm, 2); //pops key and val before the nex iteration
+  }
+  sq_pop(vm, 1);
+
+  return value;
+}
+
+template<>
+std::vector<SQBool> unpack<std::vector<SQBool>>(HSQUIRRELVM vm, SQInteger idx)
+{
+  return unpack_array<SQBool>(vm, idx);
+}
+
+template<>
+std::vector<SQInteger> unpack<std::vector<SQInteger>>(HSQUIRRELVM vm, SQInteger idx)
+{
+  return unpack_array<SQInteger>(vm, idx);
+}
+
+template<>
+std::vector<SQFloat> unpack<std::vector<SQFloat>>(HSQUIRRELVM vm, SQInteger idx)
+{
+  return unpack_array<SQFloat>(vm, idx);
+}
+
+template<>
+std::vector<SQChar const*> unpack<std::vector<SQChar const*>>(HSQUIRRELVM vm, SQInteger idx)
+{
+  return unpack_array<SQChar const*>(vm, idx);
 }
 
 template<typename T, typename... Args>
--- tests/unpack_test.cpp
+++ tests/unpack_test.cpp
@@ -19,18 +19,33 @@ TEST(SquipUnpack, unpack)
   sq_pushfloat(vm, 0.125);
   sq_pushstring(vm, "HelloWorld", -1);
 
+  sq_newarray(vm, 0);
+  sq_pushinteger(vm, 11);
+  sq_arrayappend(vm, -2);
+  sq_pushinteger(vm, 22);
+  sq_arrayappend(vm, -2);
+  sq_pushinteger(vm, 33);
+  sq_arrayappend(vm, -2);
+
   EXPECT_EQ(squip::unpack<SQBool>(vm, 2), true);
   EXPECT_EQ(squip::unpack<SQInteger>(vm, 3), 45);
   EXPECT_EQ(squip::unpack<SQFloat>(vm, 4), 0.125);
   EXPECT_STREQ(squip::unpack<char const*>(vm, 5), "HelloWorld");
+  EXPECT_EQ(squip::unpack<std::vector<SQInteger> >(vm, 6),
+            std::vector<SQInteger>({11, 22, 33}));
 
-  auto [a, b, c, d] = squip::unpack_args<SQBool, SQInteger, SQFloat, SQChar const*>(vm);
+  auto [a, b, c, d, e] = squip::unpack_args<SQBool,
+                                            SQInteger,
+                                            SQFloat,
+                                            SQChar const*,
+                                            std::vector<SQInteger>>(vm);
   EXPECT_EQ(a, true);
   EXPECT_EQ(b, 45);
   EXPECT_EQ(c, 0.125);
   EXPECT_STREQ(d, "HelloWorld");
+  EXPECT_EQ(e, std::vector<SQInteger>({11, 22, 33}));
 
-  sq_pop(vm, 5);
+  sq_pop(vm, 6);
 }
 
 /* EOF */

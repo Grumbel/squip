0839f9885d4af6cb7a35935fa7c40d125e73a3bd
Ingo Ruhnke <grumbel@gmail.com>
2023-07-31 17:27:08 +0200

    Add forward declarations header

---
+++ include/squip/fwd.hpp
@@ -0,0 +1,32 @@
+// squip - Utilities for Squirrel
+// Copyright (C) 2023 Ingo Ruhnke <grumbel@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef HEADER_SQUIP_FWD_HPP
+#define HEADER_SQUIP_FWD_HPP
+
+namespace squip {
+
+class ArrayContext;
+class Object;
+class SquirrelError;
+class SquirrelVM;
+class StackGuard;
+class TableContext;
+class Thread;
+
+} // namespace squip
+
+#endif
